2736570dc822024680c194327740f34a87986309
franklyd <franklyd@163.com>
2017-02-28 23:00:34 +0100

    feat: one-hot multi-class logistic classifier

--- exercise3/ex3/lrCostFunction.m
+++ exercise3/ex3/lrCostFunction.m
@@ -36,7 +36,11 @@ grad = zeros(size(theta));
 %           grad = grad + YOUR_CODE_HERE (using the temp variable)
 %
 
-
+Hx = sigmoid(X*theta);
+theta_temp = theta;
+theta_temp(1) = 0;
+J = -1/m*sum(log(Hx).*y + (1-y).*log(1-Hx))+sum(lambda/(2*m)*(theta_temp.^2));
+grad = 1/m*(X'*(Hx - y) + lambda*theta_temp);
 
 
 
